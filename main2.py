664e62c11a12fc9ee7310c1d5000c182c9b2fb1b
Hulu <hulumulu680@gmail.com>
2025-12-18 18:35:47 +0300

    Сделал несколько вкладок, добавил поле регистрации

---
+++ main2.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello Flask'
+
+@app.route('/user/<int:user_id>/')
+def user_profile(user_id):
+    return "Profile page of user #{}".format(user_id)
+
+@app.route('/books/<genre>/')
+def books(genre):
+    return "All Books in {} category".format(genre)
+
+@app.route('/login/', methods=['post', 'get'])
+def login():
+    message = ''
+    if request.method == 'POST':
+        username = request.form.get('username')  # запрос к данным формы
+        password = request.form.get('password')
+
+        if username == 'root' and password == 'pass':
+            message = "Correct username and password"
+    else:
+        message = "Wrong username or password"
+
+    return render_template('login.html', message=message)
+
+if __name__ == '__main__':
+    app.run(debug=True)
