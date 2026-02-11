5f4afd5f53f4a369c5a16614c7482889a16c0065
Hulu <hulumulu680@gmail.com>
2026-02-11 14:00:12 +0300

    незначительные изменения

--- app/api/v1/chat.py
+++ app/api/v1/chat.py
@@ -16,6 +16,7 @@ from sqlalchemy import desc
 # Импорты из services (после переноса логики)
 from app.services.gigachat.giga_text import response_gigachat
 from app.services.salute.salute_speech import speech_syntesis
+from env.pyparsing import replaced_by_pep8
 
 chat_v1 = Blueprint('chat_v1', __name__, url_prefix='/api/v1')
 
