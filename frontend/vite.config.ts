be70d3a5136ea1d56c33e490c2c3c07dc887819a
ila <deianeir@student.21-school.ru>
2026-03-01 23:21:33 +0300

    try fix websockets

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,21 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
+plugins: [react()],
+server: {
+    host: '0.0.0.0',        // слушать все интерфейсы
+    port: 5173,              // порт фронта
     proxy: {
       // Все запросы к /api идут на Flask
       '/api': {
-        target: 'http://127.0.0.1:5001',
-        changeOrigin: true,
+          target: 'http://178.72.164.141:5001',
+          changeOrigin: true,
       },
       // Все запросы к /static идут на Flask
       '/static': {
-        target: 'http://127.0.0.1:5001',
-        changeOrigin: true,
+          target: 'http://178.72.164.141:5001',
+          changeOrigin: true,
       }
     },
-    port: 5173,  // React порт
   },
   build: {
     // Собираем в папку static Flask
