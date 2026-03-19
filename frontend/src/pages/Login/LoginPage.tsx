4eef03a5a948b8853e33884b9ebf8b9222b312e6
ila <deianeir@student.21-school.ru>
2026-03-19 23:24:50 +0300

    temporary logs

--- frontend/src/pages/Login/LoginPage.tsx
+++ frontend/src/pages/Login/LoginPage.tsx
@@ -30,6 +30,8 @@ export default function LoginPage() {
       const data = await response.json();
       alert('Ответ получен! Токен: ' + data.data.access_token);
       localStorage.setItem('access_token', data.data.access_token);
+      console.log('🔥 data from server:', data);
+
 
       if (!response.ok) {
         // Разные сообщения в зависимости от ошибки бэкенда
